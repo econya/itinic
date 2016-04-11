b1255975603aca66d5423b6af9a442e31c5a28db
Felix Wolfsteller <felix.wolfsteller@gmail.com>
2016-04-11 14:23:46 +0200

    update schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160411085316) do
+ActiveRecord::Schema.define(version: 20160411120734) do
+
+  create_table "communities", force: :cascade do |t|
+    t.string   "name"
+    t.string   "slug"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "hackers", force: :cascade do |t|
     t.string   "first_name"
