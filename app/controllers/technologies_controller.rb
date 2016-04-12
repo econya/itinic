a516d8dd15cf7a9e1aa8c7fbff027ccd0c5f1af5
Felix Wolfsteller <felix.wolfsteller@gmail.com>
2016-04-12 09:12:19 +0200

    add technology resource views, controller

---
+++ app/controllers/technologies_controller.rb
@@ -0,0 +1,54 @@
+class TechnologiesController < ApplicationController
+  before_action :set_technology, only: [:show, :edit, :update, :destroy]
+
+  def index
+    @technologies = Technology.all
+  end
+
+  def show
+  end
+
+  def new
+    @technology = Technology.new
+  end
+
+  def edit
+  end
+
+  def create
+    @technology = Technology.new(technology_params)
+    if @technology.save
+      redirect_to @technology, notice: "Technology was successfully created."
+    else
+      render :new
+    end
+  end
+
+  def update
+    if @technology.update(technology_params)
+      redirect_to technologies_path, notice: 'Technology was successfully updated.'
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    if @technology.destroy
+      redirect_to technologies_url, notice: 'Technology was successfully deleted'
+    else
+      redirect_to technologies_url, notice: 'Technology could not be deleted.'
+    end
+  end
+
+  private
+
+  def set_technology
+    @technology = Technology.friendly.find(params[:id])
+  end
+
+  def technology_params
+    params.require(:technology).permit(
+      :name,
+      :_destroy)
+  end
+end
