e2994b75363b962808d702b23c598c965ed6f383
Felix Wolfsteller <felix.wolfsteller@gmail.com>
2016-04-14 16:53:53 +0200

    update schema after last migration

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160412191301) do
+ActiveRecord::Schema.define(version: 20160414073052) do
 
   create_table "communities", force: :cascade do |t|
     t.string   "name"
@@ -63,6 +63,18 @@ ActiveRecord::Schema.define(version: 20160412191301) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
+
+  create_table "tool_uses", force: :cascade do |t|
+    t.integer  "community_id"
+    t.integer  "technology_id"
+    t.integer  "assessment"
+    t.integer  "task"
+    t.datetime "created_at",    null: false
+    t.datetime "updated_at",    null: false
+  end
+
+  add_index "tool_uses", ["community_id"], name: "index_tool_uses_on_community_id"
+  add_index "tool_uses", ["technology_id"], name: "index_tool_uses_on_technology_id"
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
