dc9b89ce571fe00039c4ef94c1fea8198f5f6828
Felix Wolfsteller <felix.wolfsteller@gmail.com>
2016-04-14 09:27:19 +0200

    render stars for enums

--- app/helpers/enum_helper.rb
+++ app/helpers/enum_helper.rb
@@ -9,4 +9,22 @@ module EnumHelper
       [sym_to_s(k), k]
     end.to_h
   end
+
+  def render_stars value, values
+    content_tag(:div) do
+      if value.nil?
+        values.length.times.each do |v|
+          concat(fa_icon "star-o")
+        end
+      else
+        values.each do |k,v|
+          if v <= values[value]
+            concat(fa_icon "star")
+          elsif v > values[value]
+            concat(fa_icon "star-o")
+          end
+        end
+      end
+    end
+  end
 end
